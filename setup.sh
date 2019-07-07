1fcc4aae56c50eb4852780168866e27c5e0c10b8
Joakim Hedlund <contact@joakimhedlund.com>
2019-07-07 13:28:33 +0200

    Fixes sonarr/radarr basepaths, proxy password, nginx server_name

--- setup.sh
+++ setup.sh
@@ -10,8 +10,12 @@ mkdir -p ./letsencrypt/certs
 
 # Some other folders we'll be using.
 mkdir -p qbittorrent
+
 mkdir -p radarr
+echo '<Config><UrlBase>/radarr</UrlBase></Config>' > ./radarr/config.xml
+
 mkdir -p sonarr
+echo '<Config><UrlBase>/sonarr</UrlBase></Config>' > ./sonarr/config.xml
 
 echo "What domain would you like to use? Remember, it has to be managed through Cloudflare."
 read -p 'Domain name:' SETUP_DOMAIN
@@ -21,10 +25,11 @@ echo "Great. Lets set up a username and password for Nginx. These will be used t
 
 read -p 'Username:' SETUP_USER
 read -p 'Password:' SETUP_PASS
-echo "$SETUP_USER:{PLAIN}$SETUP_USER" >> ./nginx/htpasswd
+echo "$SETUP_USER:{PLAIN}$SETUP_PASS" >> ./nginx/htpasswd
 
 # Something to replace the default values in the nginx conf without trashing stuff like $uri
 #echo '$PATH $HOSTNAME' | envsubst '$HOSTNAME' >&1
+export SETUP_DOMAIN=$SETUP_DOMAIN
 envsubst '$SETUP_DOMAIN' < nginx/dockarr-proxy-template.conf > nginx/sites-enabled/dockarr-proxy
 
 echo "You're all set! You can boot your new Dockarr setup by running 'docker-compose up'"
